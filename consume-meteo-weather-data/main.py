5f93c1897436f3914fbe010fe4edb8c98f27be10
Sridhar Streaks <100800059+sridharstreaks@users.noreply.github.com>
2024-11-03 16:50:20 +0000

    changed consumer to sdf

--- consume-meteo-weather-data/main.py
+++ consume-meteo-weather-data/main.py
@@ -1,44 +1,23 @@
 import os
 from quixstreams import Application
-import json
 
 # for local dev, load env vars from a .env file
 from dotenv import load_dotenv
 load_dotenv()
 
-def main():
+app = Application(consumer_group="chennai-weather", auto_offset_reset="latest")
 
-    app = Application(consumer_group="chennai-weather",auto_offset_reset="latest")
+input_topic = app.topic(os.environ["input"])
+output_topic = app.topic(os.environ["output"])
 
-    input_topic = app.topic(os.environ["input"])
-    #print(input_topic.name)
-    output_topic = app.topic(os.environ["output"])
-
-    with app.get_consumer() as consumer:
-        consumer.subscribe([input_topic.name])
-
-        while True:
-            msg=consumer.poll(1)
-
-            if msg is None:
-                print("waiting...")
-            elif msg.error() is not None:
-                raise Exception(msg.error())
-            else:
-                key = msg.key().decode('utf8')
-                value = json.loads(msg.value())
-                offset = msg.offset()
-                consumer.store_offsets(msg)
-
-
-#sdf = app.dataframe(input_topic)
+sdf = app.dataframe(input_topic)
 
 # put transformation logic here
 # see docs for what you can do
 # https://quix.io/docs/get-started/quixtour/process-threshold.html
 
-#sdf.print()
-#sdf.to_topic(output_topic)
+sdf.print()
+sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
-    main()
+    app.run(sdf)
